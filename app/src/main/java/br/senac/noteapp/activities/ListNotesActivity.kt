001ebfcf695ab5d2f2b224ff646993313dd247fa
fabio.hafreire <fabio.hafreire@sp.senac.br>
2022-11-16 18:24:17 -0300

    Atualização de exemplo

--- app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
+++ app/src/main/java/br/senac/noteapp/activities/ListNotesActivity.kt
@@ -1,5 +1,6 @@
 package br.senac.noteapp.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.senac.noteapp.databinding.ActivityListNotesBinding
@@ -12,6 +13,10 @@ class ListNotesActivity : AppCompatActivity() {
         binding = ActivityListNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.fab.setOnClickListener {
+            val i = Intent(this, NewNoteActivity::class.java)
+            startActivity(i)
+        }
     }
 
 }
